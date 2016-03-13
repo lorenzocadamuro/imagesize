5dba424c619384e96121e0f2f3ebc75f5721a93e
Lorenzo Cadamuro <lorenzocadamuro@MacBook-Pro-di-Lorenzo.local>
2016-03-13 23:01:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Imagesize
+Retrieve width and height of an image before it is completely loaded.
+Module uses ES6 Promises. I raccomend the polyfill https://github.com/stefanpenner/es6-promise
+
+## Install
+
+```sh
+bower install imagesize
+```
+
+## Use
+
+```javascript
+var image = document.getElementById('foo');
+
+imagesize(image, function(image) {
+  console.log('Success', image);
+}, function() {
+  console.log('Error');
+});
+```
---
+++ imagesize.js
@@ -0,0 +1,65 @@
+"use strict";
+
+(function(root, factory) {
+  // AMD && CommonJS support
+  if (typeof define === 'function' && define['amd']) {
+    define(function() { return factory; });
+  } else if (typeof module !== 'undefined' && module['exports']) {
+    module['exports'] = factory;
+  } else if (typeof this !== 'undefined') {
+    root['imagesize'] = factory;
+  }
+})(this, function(value, onSuccess, onError) {
+  onSuccess = typeof onSuccess === 'function' ? onSuccess : function() {};
+  onError = typeof onError === 'function' ? onError : function() {};
+
+  var src = (function(value) {
+    if (value) {
+      if (typeof value === 'object' && value.nodeType === 1) {
+        if ('src' in value) {
+          return value.currentSrc || value.src;
+        } else {
+          return null;
+        }
+      } else {
+        return value;
+      }
+    }
+  })(value);
+
+  var promise = new Promise(function(resolve, reject) {
+    if (typeof src === 'string') {
+      var image = new Image();
+
+      var cb = function() {
+        resolve({
+          src: src,
+          width: image.width,
+          height: image.height
+        });
+
+        image.src = '';
+      };
+
+      image.onload = cb;
+      image.onerror = reject;
+      image.src = src;
+
+      var wait = function() {
+        if (!image.complete) {
+          if (image.width && image.height) {
+            cb();
+          } else {
+            setTimeout(wait);
+          }
+        }
+      };
+
+      wait();
+    } else {
+      reject();
+    }
+  });
+
+  promise.then(onSuccess, onError);
+});
